2f31c30dcf8649c3d4a77cf12650eda2b2d28ed2
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-02-18 17:05:27 +0100

    Fix unused import

--- backend/storage/editor/views.py
+++ backend/storage/editor/views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from scenario.models import Room
-from editor.models import Template, TemplateLink, Variable, Rule, CONTEXTS
+from editor.models import Template, TemplateLink, Variable, Rule
 
 
 def get_serialized_templates():
