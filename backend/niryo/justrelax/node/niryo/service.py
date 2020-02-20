9b2eb9dfc7f8734d5b5385b0734cfd3f3ad9c373
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-02-20 17:36:45 +0100

    Add new Niryo node

--- backend/niryo/justrelax/node/niryo/service.py
+++ backend/niryo/justrelax/node/niryo/service.py
@@ -2,157 +2,78 @@ import rospy
 
 from niryo_one_python_api.niryo_one_api import NiryoOne, TOOL_GRIPPER_1_ID
 
+from justrelax.common.logging_utils import logger
 from justrelax.node.service import JustSockClientService
 
 
-class Joint(object):
-    def __init__(self):
-        self._raw_target_position = 0.
-        self._raw_current_position = 0.
-        self._target_position = 0.
-        self._current_position = 0.
-
-    @property
-    def raw_target_position(self):
-        return self._raw_target_position
-
-    @raw_target_position.setter
-    def raw_target_position(self, value):
-        self._raw_target_position = value
-
-    @property
-    def raw_current_position(self):
-        return self._raw_current_position
-
-    @raw_current_position.setter
-    def raw_current_position(self, value):
-        self._raw_current_position = value
-
-    @property
-    def target_position(self):
-        return self._target_position
-
-    @target_position.setter
-    def target_position(self, value):
-        self._target_position = value
-
-    @property
-    def current_position(self):
-        return self._current_position
-
-    @current_position.setter
-    def current_position(self, value):
-        self._current_position = value
-
-    def set_position_from_raw_value(self, raw_value):
-        self.raw_target_position = raw_value
-        self.raw_current_position = raw_value
-
-    def set_target(self, value):
-        self.raw_target_position = value
-
-    def get_raw_delta_target(self):
-        return min(self.raw_target_position - self.current_position, 0.01)
-
-
-class JointsController(object):
-    JOINT_1 = 0
-    JOINT_2 = 1
-    JOINT_3 = 2
-    JOINT_4 = 3
-    JOINT_5 = 4
-    JOINT_6 = 5
-
-    def __init__(self, niryo_one_controller):
-        self.noc = niryo_one_controller
-        self.jcs = {
-            JointsController.JOINT_1: Joint(),
-            JointsController.JOINT_2: Joint(),
-            JointsController.JOINT_3: Joint(),
-            JointsController.JOINT_4: Joint(),
-            JointsController.JOINT_5: Joint(),
-            JointsController.JOINT_6: Joint(),
-        }
-
-    def set_positions_from_raw_values(self, raw_values):
-        # Using enumerate is a shortcut because joint ids have been chosen
-        # to match the index of the values provided in the list returned by
-        # the NiryoOne.get_joints method.
-        for joint_id, raw_value in enumerate(raw_values):
-            self.jcs[joint_id].set_position_from_raw_value(raw_value)
-
-    def set_joint_target(self, joint_id, value):
-        self.jcs[joint_id].set_target(value)
-
-        joints = [j.raw_current_position() for j in self.jcs.values()]
-
-        delta = self.jcs[joint_id].get_raw_delta_target()
-        while delta > 0.01:
-            delta = self.jcs[joint_id].get_raw_delta_target()
-            joints[joint_id] = delta
-            self.noc.move_joints(joints)
-
-        # over time
-        # raw_delta_targets = [j.get_raw_delta_target() for j in self.jcs.values()]
-        # self.noc.move_joints(raw_delta_targets)
-
-
-class NiryoOneController(NiryoOne):
-    GRIPPER = TOOL_GRIPPER_1_ID
-    GRIPPER_SPEED = 500
-
-    def __init__(self):
-        NiryoOne.__init__(self)
-        self.joc = JointsController(self)
-
-    @property
-    def need_calibration(self):
-        return bool(self.get_hardware_status().calibration_needed)
-
-    def activate_motors(self):
-        self.activate_learning_mode(False)
-        raw_values = self.get_joints()
-        self.joc.set_positions_from_raw_values(raw_values)
-
-    def deactivate_motors(self):
-        self.activate_learning_mode(True)
-
-    def init_gripper(self):
-        self.change_tool(self.GRIPPER)
-
-    def open_gripper(self):
-        self.instance.open_gripper(self.GRIPPER, self.GRIPPER_SPEED)
-
-    def close_gripper(self):
-        self.instance.close_gripper(self.GRIPPER, self.GRIPPER_SPEED)
-
-    def move_joint(self, joint_id, target):
-        self.joc.set_joint_target(joint_id, target)
-
-
 class Niryo(JustSockClientService):
-    def __init__(self, name, channel, *args, **kwargs):
-        super(Niryo, self).__init__(*args, **kwargs)
-        rospy.init_node('{}@{}'.format(name, channel))
-        self.noc = NiryoOneController()
-
-        if self.noc.need_calibration:
-            self.noc.calibrate_auto()
-
-        if self.noc_need_calibration:
-            self.noc.calibrate_manual()
-
-        if self.noc_need_calibration:
-            """problem"""
-
-        self.init_gripper()
-
     def start(self):
-        self.noc.activate_motors()
+        rospy.init_node('justrelax')
+        self.niryo = NiryoOne()
+        self.niryo.change_tool(TOOL_GRIPPER_1_ID)
+
+    def get_joints_from_position_name(self, name):
+        positions = self.niryo.get_saved_position_list()
+        for position in positions:
+            if position.name == name:
+                return position.joints
 
     def process_event(self, event):
-        self.factory.send_message(event)
-        self.move_joint(JointsController.JOINT_1, )
+        logger.debug("Processing event '{}'".format(event))
+        if type(event) is not dict:
+            logger.debug("Unknown event: skipping")
+            return
 
-    def stop(self):
-        self.noc.deactivate_motors()
+        if "action" not in event:
+            logger.debug("Event has no action: skipping")
+            return
+
+        if event["action"] == "calibrate":
+            auto = event.get("auto", False)
+            if auto:
+                logger.info("Start an auto calibration")
+                self.niryo.calibrate_auto()
+            else:
+                logger.info("Start a manual calibration")
+                self.niryo.calibrate_manual()
+            logger.info("Calibration finished")
+
+        elif event["action"] == "configure":
+            if "learning_mode" in event:
+                if event["learning_mode"]:
+                    logger.info("Activate learning mode")
+                    self.niryo.activate_learning_mode(True)
+                else:
+                    logger.info("Deactivate learning mode")
+                    self.niryo.activate_learning_mode(False)
+            if "velocity" in event:
+                velocity = int(event["velocity"])
+                assert 1 <= velocity <= 100, "Speed must be between 1 and 100"
+                logger.info("Set velocity to {}".format(velocity))
+                self.niryo.set_arm_max_velocity(velocity)
+
+        elif event["action"] == "move_position":
+            position_name = str(event.get("position", ""))
+            joints = self.get_joints_from_position_name(position_name)
+            self.niryo.move_joints(joints)
+
+        elif event["action"] == "move_joint":
+            joint = int(event.get("joint", 0))
+            assert 0 <= joint <= 5, "Joint must be between 0 and 5"
+            position_name = str(event.get("position", ""))
+
+            position_joints = self.get_joints_from_position_name(position_name)
+            joints = self.niryo.joints[:]
+
+            joints[joint] = position_joints[joint]
+
+            self.niryo.move_joints(joints)
+
+        elif event["action"] == "open_gripper":
+            self.niryo.open_gripper(TOOL_GRIPPER_1_ID, 350)
+
+        elif event["action"] == "close_gripper":
+            self.niryo.close_gripper(TOOL_GRIPPER_1_ID, 350)
+
+        else:
+            logger.debug("Unknown action type '{}': skipping".format(event["action"]))
