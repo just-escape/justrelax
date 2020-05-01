f71e96c58d4220c29e12fec952107992f98bfced
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-05-01 15:35:34 +0200

    Update the table node according to the new electronic plan

--- contrib/digimiam/table/justrelax/node/table/service.py
+++ contrib/digimiam/table/justrelax/node/table/service.py
@@ -1,3 +1,4 @@
+import time
 from gpiozero import OutputDevice, InputDevice
 
 from twisted.internet.reactor import callLater
@@ -7,11 +8,36 @@ from justrelax.common.logging_utils import logger
 from justrelax.node.service import JustSockClientService, EventCategoryToMethodMixin
 
 
+class Motor:
+    def __init__(self, up_pin, down_pin):
+        # Never have those the two pins active at the same time or the electronics might crash
+        self.motor_up = OutputDevice(up_pin)
+        self.motor_down = OutputDevice(down_pin)
+        self.motor_up.off()
+        self.motor_down.off()
+
+    def position_up(self):
+        self.motor_down.off()
+        time.sleep(0.1)
+        self.motor_up.on()
+
+    def position_down(self):
+        self.motor_up.off()
+        time.sleep(0.1)
+        self.motor_down.on()
+
+    def is_position_down(self):
+        return self.motor_down.is_active
+
+    def is_position_up(self):
+        return self.motor_up.is_active
+
+
 class Table(EventCategoryToMethodMixin, JustSockClientService):
     def __init__(self, *args, **kwargs):
         super(Table, self).__init__(*args, **kwargs)
         self.switch = InputDevice(self.node_params["switch_pin"])
-        self.motor = OutputDevice(self.node_params["motor_pin"])
+        self.motor = Motor(self.node_params["motor_up_pin"], self.node_params["motor_down_pin"])
         self.led = OutputDevice(self.node_params["led_pin"])
         self.led_delay = self.node_params["led_delay"]
         self.led_task = None
@@ -20,8 +46,8 @@ class Table(EventCategoryToMethodMixin, JustSockClientService):
 
     def check_switch(self):
         if self.switch.is_active:
-            if not self.motor.is_active:
-                logger.info("Switch is active and motor is not active")
+            if not self.motor.is_position_up:
+                logger.info("Switch is active and motor is in position up")
                 self.event_down()
                 self.check_switch_task.stop()
 
@@ -30,23 +56,25 @@ class Table(EventCategoryToMethodMixin, JustSockClientService):
             self.led_task.cancel()
 
     def event_reset(self):
-        logger.info("Resetting node: turning on motor, turning off led and listening to the switch.")
-        self.motor.on()
+        logger.info("Resetting: setting the motor in up position, turning off the led and listening to the switch.")
+        self.motor.position_up()
         self.led.off()
         self.cancel_led_task()
         if not self.check_switch_task.running:
             self.check_switch_task.start(1 / 25)
 
     def event_up(self):
-        logger.info("Pulling table up: turning on motor and turning off led. The switch will not be listened.")
-        self.motor.on()
+        logger.info(
+            "Pulling table up: setting the motor in up position and turning off the led. Don't listen to the switch.")
+        self.motor.position_up()
         self.led.off()
         self.cancel_led_task()
 
     def event_down(self):
         logger.info(
-            "Pulling table down: turning off motor, scheduling to turn on the led and stop listening to the switch.")
-        self.motor.off()
+            "Pulling table down: setting the motor in down position, scheduling to turn on the led and stop listening "
+            "to the switch.")
+        self.motor.position_down()
         self.cancel_led_task()
         self.led_task = callLater(self.led_delay, self.led.on)
         self.check_switch_task.stop()
