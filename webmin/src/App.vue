a98015d8f0519c9eaeece30a3be44a707a19a43d
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-07-01 10:03:02 +0200

    Add url parameter to configure the websocket url

--- webmin/src/App.vue
+++ webmin/src/App.vue
@@ -16,7 +16,12 @@ export default {
   components: {Nav},
   created() {
     roomStore.dispatch('fetchScenarios')
-    this.$connect()
+    let ws_url = this.$route.query.ws_url
+    if (ws_url) {
+      this.$connect('ws://' + ws_url)
+    } else {
+      this.$connect('ws://localhost:3031')
+    }
   }
 }
 </script>
