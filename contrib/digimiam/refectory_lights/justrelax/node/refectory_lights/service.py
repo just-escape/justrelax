d555bbc242607b928f5ff4808f703182b7cc3b4d
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-07-08 15:19:43 +0200

    Fix

--- contrib/digimiam/refectory_lights/justrelax/node/refectory_lights/service.py
+++ contrib/digimiam/refectory_lights/justrelax/node/refectory_lights/service.py
@@ -9,7 +9,7 @@ class RefectoryLights(EventCategoryToMethodMixin, JustSockClientService):
         super(RefectoryLights, self).__init__(*args, **kwargs)
 
         self.colors = {}
-        for color, conf in self.node_params['colors'].values():
+        for color, conf in self.node_params['colors'].items():
             self.colors[color] = OutputDevice(conf['pin'])
             if not conf.get('off_by_default', False):
                 self.colors[color].on()
