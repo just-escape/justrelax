428c61e47e8e9bb8601fd04b4e6165357a7fdfbe
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-07-08 19:29:06 +0200

    Fix algorithm

--- contrib/digimiam/refectory_lights/arduino/refectory_lights.ino
+++ contrib/digimiam/refectory_lights/arduino/refectory_lights.ino
@@ -6,21 +6,21 @@ unsigned long deltaTimeModulus1000;
 unsigned long lastCycleStart;
 
 // White, Purple, Red, Green, Blue, Orange
-int lightPins[CHANNELS][3] = {{2, -1, -1}, {3, -1, -1}, {4, -1, -1}, {5, -1, -1}, {6, 7, 8}, {9, 10, 11}};
-int brightnesses[CHANNELS][3] = {{300, 0, 0}, {300, 0, 0}, {300, 0, 0}, {300, 0, 0}, {0, 120, 300}, {600, 72, 0}};
-bool isOnPin[CHANNELS] = {48, 52, 46, 50, 42, 44};
+int lightPins[CHANNELS][3] = {{2, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
+int brightnesses[CHANNELS][3] = {{30, 0, 0}, {300, 0, 0}, {300, 0, 0}, {300, 0, 0}, {0, 120, 300}, {600, 72, 0}};
+byte isOnPin[CHANNELS] = {48, 52, 46, 50, 42, 44};
+bool isHigh[CHANNELS][3] = {{false}};
 
 void setup() {
-  for (int i = 0 ; i < CHANNELS ; i++) {
+  for (byte i = 0 ; i < CHANNELS ; i++) {
     pinMode(isOnPin[i], INPUT);
-    for (int j = 0 ; j < 3 ; j++) {
+    for (byte j = 0 ; j < 3 ; j++) {
       if (lightPins[i][j] != -1) {
         pinMode(lightPins[i][j], OUTPUT);
       }
     }
   }
 
-  Serial.begin(9600);
   lastCycleStart = micros();
 }
 
@@ -31,21 +31,24 @@ void loop() {
   if (deltaTime > 1000) {
     lastCycleStart = currentTime;
 
-    for (int i = 0 ; i < CHANNELS ; i++) {
-      for (int j = 0 ; j < 3 ; j++) {
-        digitalWrite(lightPins[i][j], digitalRead(isOnPin[i]));
+    for (byte i = 0 ; i < CHANNELS ; i++) {
+      for (byte j = 0 ; j < 3 ; j++) {
+        if (lightPins[i][j] != -1) {
+          isHigh[i][j] = digitalRead(qqch);
+        }
       }
     }
   }
 
   deltaTimeModulus1000 = deltaTime % 1000;
 
-  for (int i = 0 ; i < CHANNELS ; i++) {
-    for (int j = 0 ; j < 3 ; j++) {
+  for (byte i = 0 ; i < CHANNELS ; i++) {
+    for (byte j = 0 ; j < 3 ; j++) {
       if (lightPins[i][j] != -1) {
         if (deltaTimeModulus1000 > brightnesses[i][j]) {
-          digitalWrite(lightPins[i][j], false);
+          isHigh[i][j] = false;
         }
+        digitalWrite(lightPins[i][j], isHigh[i][j]);
       }
     }
   }
