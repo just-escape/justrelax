a0a9fb0f600963abac864d8bde45c40462631f8a
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-07-14 19:40:02 +0200

    Update configuration from tests

--- contrib/digimiam/refectory_lights/arduino/refectory_lights.ino
+++ contrib/digimiam/refectory_lights/arduino/refectory_lights.ino
@@ -6,8 +6,8 @@ unsigned long deltaTimeModulus1000;
 unsigned long lastCycleStart;
 
 // White, Purple, Red, Green, Blue, Orange
-int lightPins[CHANNELS][3] = {{2, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
-int brightnesses[CHANNELS][3] = {{30, 0, 0}, {300, 0, 0}, {300, 0, 0}, {300, 0, 0}, {0, 120, 300}, {600, 72, 0}};
+int lightPins[CHANNELS][3] = {{2, -1, -1}, {3, -1, -1}, {4, -1, -1}, {5, -1, -1}, {6, 7, 8}, {9, 10, 11}};
+int brightnesses[CHANNELS][3] = {{30, 0, 0}, {300, 0, 0}, {300, 0, 0}, {300, 0, 0}, {0, 120, 300}, {600, 116, 0}};
 byte isOnPin[CHANNELS] = {48, 52, 46, 50, 42, 44};
 bool isHigh[CHANNELS][3] = {{false}};
 
@@ -34,7 +34,7 @@ void loop() {
     for (byte i = 0 ; i < CHANNELS ; i++) {
       for (byte j = 0 ; j < 3 ; j++) {
         if (lightPins[i][j] != -1) {
-          isHigh[i][j] = digitalRead(qqch);
+          isHigh[i][j] = digitalRead(isOnPin[i]);
         }
       }
     }
