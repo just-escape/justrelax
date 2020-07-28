f2f645d4c79ac6861cd66caf9842e1e6285f42d0
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-07-28 11:41:38 +0200

    Fix typo

--- backend/video_player/setup.py
+++ backend/video_player/setup.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     setup(
         name="justrelax_node_video_player",
         version="0.1",
-        packages=["justrelax.node.hologram_player", "twisted.plugins"],
+        packages=["justrelax.node.video_player", "twisted.plugins"],
         install_requires=get_requirements(),
         zip_safe=False,
     )
