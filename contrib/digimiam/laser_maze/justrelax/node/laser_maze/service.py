b4f5ad2e200599fbff9d91bee77054899734a0c5
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2020-11-28 14:52:54 +0100

    Update laser maze service

--- contrib/digimiam/laser_maze/justrelax/node/laser_maze/service.py
+++ contrib/digimiam/laser_maze/justrelax/node/laser_maze/service.py
@@ -42,13 +42,31 @@ class LaserMaze(EventCategoryToMethodMixin, JustSockClientService):
         ALARM_LASER_INDEX = "i"
         TAG = "t"
         TAG_VALUE = "v"
+        TAG_READER_INDEX = "i"
 
         LASER_ON = "l"
         LASER_ON_BITMASK = "b"
+        DYNAMIC_LASER_ON_BITMASK = "d"
+        DYNAMIC_LASER_UPTIME = "p"
+        DYNAMIC_LASER_DOWNTIME = "n"
+        DYNAMIC_LASER_INCREMENTAL_OFFSET = "o"
         STOP_PLAYING = "s"
+        SET_SUCCESS = "w"
+        SET_SUCCESS_VALUE = "v"
 
     def __init__(self, *args, **kwargs):
         super(LaserMaze, self).__init__(*args, **kwargs)
+
+        self.success = False
+
+        self.laser_prefix = self.node_params['laser_prefix']
+
+        if self.node_params['tag_readers'] is None:
+            self.tag_readers = None
+        else:
+            self.tag_readers = {}
+            for reader_index in self.node_params['tag_readers']:
+                self.tag_readers[reader_index] = False
 
         self.default_difficulty = self.node_params['default_difficulty']
         self._difficulty = None
@@ -77,8 +95,13 @@ class LaserMaze(EventCategoryToMethodMixin, JustSockClientService):
 
         self._difficulty = value
 
-        difficulty_laser_bitmask = self.difficulty_settings[value]
-        self.event_set_laser_bitmask(difficulty_laser_bitmask)
+        bitmask = self.difficulty_settings[value]['laser_bitmask']
+        dynamic_bitmask = self.difficulty_settings[value]['dynamic_laser_bitmask']
+        dynamic_downtime = self.difficulty_settings[value]['dynamic_laser_downtime']
+        dynamic_uptime = self.difficulty_settings[value]['dynamic_laser_uptime']
+        dynamic_incremental_offset = self.difficulty_settings[value]['dynamic_laser_incremental_offset']
+        self.event_laser_on(
+            bitmask, dynamic_bitmask, dynamic_downtime, dynamic_uptime, dynamic_incremental_offset)
 
     def process_serial_event(self, event):
         logger.debug("Processing event '{}'".format(event))
@@ -96,30 +119,52 @@ class LaserMaze(EventCategoryToMethodMixin, JustSockClientService):
             self.on_alarm(laser_index)
 
         elif event[self.ARDUINO_PROTOCOL.CATEGORY] == self.ARDUINO_PROTOCOL.TAG:
+            if self.ARDUINO_PROTOCOL.TAG_READER_INDEX not in event:
+                logger.error("Event has no tag reader index: skipping")
+                return
+
             if self.ARDUINO_PROTOCOL.TAG_VALUE not in event:
                 logger.error("Event has no tag value: skipping")
                 return
 
+            reader_index = event[self.ARDUINO_PROTOCOL.TAG_READER_INDEX]
             tag = event[self.ARDUINO_PROTOCOL.TAG_VALUE]
-            self.on_tag_read(tag)
+            self.on_tag_read(reader_index, tag)
 
         else:
             logger.error("Unknown event category '{}': skipping".format(self.ARDUINO_PROTOCOL.CATEGORY))
 
-    def on_tag_read(self, tag):
-        logger.info("Tag read={}".format(tag))
+    def on_tag_read(self, reader_index, tag):
+        logger.info("Tag read={} from reader index={}".format(tag, reader_index))
+        if self.tag_readers is None:
+            logger.info("Tag readers has been disabled: skipping")
+            return
+
+        self.tag_readers[reader_index] = tag
+        if all(self.tag_readers.values()):
+            logger.info("All tags are enabled: triggering success")
+            self.send_event({"category": "success"})
 
     def on_alarm(self, laser_index):
         logger.info("Alarm from laser index={}".format(laser_index))
+        self.send_event({"category": "alarm", "laser": "{}{}".format(self.laser_prefix, laser_index)})
 
     def init_arduino(self):
         self.difficulty = self.default_difficulty
 
-    def event_set_laser_bitmask(self, bitmask: int):
+    def event_laser_on(
+            self, bitmask: int, dynamic_bitmask: int, dynamic_downtime: int,
+            dynamic_uptime: int, dynamic_incremental_offset: int,
+    ):
+        logger.info("Set bitmask={}".format(bitmask))
         self.buffer.send_event(
             {
                 self.ARDUINO_PROTOCOL.CATEGORY: self.ARDUINO_PROTOCOL.LASER_ON,
                 self.ARDUINO_PROTOCOL.LASER_ON_BITMASK: bitmask,
+                self.ARDUINO_PROTOCOL.DYNAMIC_LASER_ON_BITMASK: dynamic_bitmask,
+                self.ARDUINO_PROTOCOL.DYNAMIC_LASER_DOWNTIME: dynamic_downtime,
+                self.ARDUINO_PROTOCOL.DYNAMIC_LASER_UPTIME: dynamic_uptime,
+                self.ARDUINO_PROTOCOL.DYNAMIC_LASER_INCREMENTAL_OFFSET: dynamic_incremental_offset,
             }
         )
 
@@ -127,11 +172,33 @@ class LaserMaze(EventCategoryToMethodMixin, JustSockClientService):
         self.difficulty = difficulty
 
     def event_stop_playing(self):
+        logger.info("Stop playing")
         self.buffer.send_event(
             {
                 self.ARDUINO_PROTOCOL.CATEGORY: self.ARDUINO_PROTOCOL.STOP_PLAYING,
             }
         )
 
+    def event_playing(self):
+        bitmask = self.difficulty_settings[self.difficulty]['laser_bitmask']
+        dynamic_bitmask = self.difficulty_settings[self.difficulty]['dynamic_laser_bitmask']
+        dynamic_downtime = self.difficulty_settings[self.difficulty]['dynamic_laser_downtime']
+        dynamic_uptime = self.difficulty_settings[self.difficulty]['dynamic_laser_uptime']
+        dynamic_incremental_offset = self.difficulty_settings[self.difficulty]['dynamic_laser_incremental_offset']
+        self.event_laser_on(
+            bitmask, dynamic_bitmask, dynamic_downtime, dynamic_uptime, dynamic_incremental_offset)
+
+    def event_set_success(self, value: bool):
+        logger.info("Setting success={}".format(value))
+        self.success = value
+        self.buffer.send_event(
+            {
+                self.ARDUINO_PROTOCOL.CATEGORY: self.ARDUINO_PROTOCOL.SET_SUCCESS,
+                self.ARDUINO_PROTOCOL.SET_SUCCESS_VALUE: value,
+            }
+        )
+
     def event_reset(self):
+        logger.info("Resetting")
+        self.event_set_success(False)
         self.difficulty = self.default_difficulty
