cd85237fc6b30b77c168269838099168eb10866f
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2021-01-28 18:30:57 +0100

    Add the possibility to send fog forever

--- contrib/digimiam/fog_machine/justrelax/node/fog_machine/service.py
+++ contrib/digimiam/fog_machine/justrelax/node/fog_machine/service.py
@@ -19,10 +19,12 @@ class FogMachine(EventCategoryToMethodMixin, JustSockClientService):
             self.event_off()
 
         self.send_fog_default_duration = self.node_params['send_fog_default_duration']
+        self.send_fog_forever_default_frequency = self.node_params['send_fog_forever_default_frequency']
         self.fog_pin = OutputDevice(self.node_params['fog_pin'])
         self.fog_pin.off()
 
         self.send_fog_task = None
+        self.send_fog_forever_task = None
 
     def event_on(self):
         logger.info("Heating machine and turning on fan")
@@ -49,3 +51,19 @@ class FogMachine(EventCategoryToMethodMixin, JustSockClientService):
             logger.info("Sending fog for {} seconds".format(delay))
             self.fog_pin.on()
             self.send_fog_task = callLater(delay, self._release_fog_pin)
+
+    def event_send_fog_forever(self, frequency: int = None, send_duration: int = None):
+        if self.send_fog_forever_task and self.send_fog_forever_task.active():
+            logger.info("Already sending fog forever: stopping previous task")
+            self.send_fog_forever_task.cancel()
+
+        frequency = frequency if frequency is not None else self.send_fog_forever_default_frequency
+        self.send_fog_forever_task = callLater(frequency, self.event_send_fog, send_duration)
+        self.event_send_fog(send_duration)
+
+    def event_stop_sending_fog_forever(self):
+        if self.send_fog_forever_task and self.send_fog_forever_task.active():
+            logger.info("Stop sending fog forever task")
+            self.send_fog_forever_task.cancel()
+        else:
+            logger.info("Fog was not already being sent forever: skipping")
