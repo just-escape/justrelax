d19b3ad8755fb0f94a9277ac474a06c9e2b30cc2
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2022-01-23 00:07:09 +0100

    Reduce maximum log size produced by nodes

--- backend/core/justrelax/core/logging_utils.py
+++ backend/core/justrelax/core/logging_utils.py
@@ -8,17 +8,18 @@ from twisted.python import log
 logger = logging.getLogger()
 
 
-def init_logging(level='INFO', file='/dev/null', twisted_logs=False):
+def init_logging(level='INFO', file='/dev/null', twisted_logs=False, stdout=False):
     l = logger
     l.setLevel(level)
 
     line_formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s", datefmt="%Y-%m-%dT%H:%M:%S%z")
 
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(line_formatter)
-    l.addHandler(stream_handler)
+    if stdout:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(line_formatter)
+        l.addHandler(stream_handler)
 
-    rotating_file_handler = logging.handlers.RotatingFileHandler(file, maxBytes=10485760, backupCount=10)
+    rotating_file_handler = logging.handlers.RotatingFileHandler(file, maxBytes=1485760, backupCount=3)
     rotating_file_handler.setFormatter(line_formatter)
     l.addHandler(rotating_file_handler)
 
