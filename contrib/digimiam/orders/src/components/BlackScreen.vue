5663fc7360b56c815e9645834ea41f5c485fb84f
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2022-05-09 00:07:11 +0200

    Fix black screen

--- contrib/digimiam/orders/src/components/BlackScreen.vue
+++ contrib/digimiam/orders/src/components/BlackScreen.vue
@@ -2,7 +2,7 @@
   <div
     class="w-100 h-100 z-index-50 position-absolute"
     style="top: 0; left: 0; transition: all 1s ease-in-out; background: black"
-    :style="{opacity: opacity}"
+    :class="{'d-none': !display}"
   >
   </div>
 </template>
@@ -13,8 +13,8 @@ import progressionStore from '@/store/progressionStore.js'
 export default {
   name: "BlackScreen",
   computed: {
-    opacity() {
-      return progressionStore.state.displayBlackScreen ? 1 : 0
+    display() {
+      return progressionStore.state.displayBlackScreen
     },
   },
 }
